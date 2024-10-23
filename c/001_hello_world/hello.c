2540c7271d9892d9b59c959bd800f302b3dd2694
Ivan Lemeshev <vanlmshv@gmail.com>
2024-10-23 21:31:01 +0300

    Clean up C code

--- c/001_hello_world/hello.c
+++ c/001_hello_world/hello.c
@@ -1,28 +1,7 @@
-// This is a preprocessor directive that includes the standard input/output
-// library in the program. This library contains functions for input and output
-// operations. In this case, it's needed for the puts() function used later in
-// the program.
 #include <stdio.h>
-
-// This is another preprocessor directive that includes the standard library in
-// the program. This library contains general purpose functions, including
-// memory management, process control, conversions and others. In this case,
-// it's needed for the EXIT_SUCCESS macro used later in the program.
 #include <stdlib.h>
 
-// This is the main function of the program. In C, execution of the program
-// starts with the main function. The void keyword indicates that this function
-// takes no arguments.
 int main(void) {
-    // This line calls the puts() function, which is a standard library function
-    // that writes a string to the console, followed by a newline character.
-    // Here, it's used to print "Hello, World!".
-    puts("Hello, World!");
-
-    // This line indicates that the program has finished successfully.
-    // EXIT_SUCCESS is a macro (defined in stdlib.h) that represents a
-    // successful program termination status. In most systems, it's defined as
-    // 0. If a C program finishes without any return statement in main, it
-    // returns a status of EXIT_SUCCESS by default.
-    return EXIT_SUCCESS;
+  puts("Hello, World!");
+  return EXIT_SUCCESS;
 }
--- c/005_scope/scope.c
+++ c/005_scope/scope.c
@@ -1,11 +1,22 @@
-int j;           // file scope of j begins
-void f(int i) {  // block scope of i begins
-    int j = 1;   // block scope of j begins; hides file-scope j
-    i++;         // i refers to the function parameter
-    for (int i = 0; i < 2; i++) {  // block scope of loop-local i begins
-        int j = 2;          // block scope of the inner j begin; hides outer j
-        printf("%d\n", j);  // inner j is in scope, prints 2
-    }                       // block scope of the inner i and j ends
-    printf("%d\n", j);      // the outer j is in scope, prints 1
-}  // the block scope of i and j ends
-void g(int j);  // j has function prototype scope; hides file-scope j
+#include <stdio.h>
+#include <stdlib.h>
+
+int j;
+
+void f(int i) {
+  int j = 1;
+  i++;
+  for (int i = 0; i < 2; i++) {
+    int j = 2;
+    printf("%d\n", j);
+  }
+  printf("%d\n", j);
+}
+
+void g(int j);
+
+int main(void) {
+  int i = 0;
+  f(i);
+  return EXIT_SUCCESS;
+}
